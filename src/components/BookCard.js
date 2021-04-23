81a2b0c348c152a432b2004c4df93bd01d12d2d5
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-04-23 11:57:25 -0700

    modal is working adding dialogs to display content about each book upon clicking more info button

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -12,17 +12,42 @@ const BookCard = ( {
         previewLink,
         infoLink
     } ) => {
-    const [modal, setModal] = useState(false);
-    const toggle = () => setModal(!modal);
+        const [show, setShow] = useState(false);
 
+        const handleClose = () => setShow(false);
+        const handleShow = () => setShow(true);
     return (
         <Card className="m-auto" style={{width: '233px'}}>
             <CardImg top style={ {width: '100%', height: '230px' }} src={ thumbnail } alt='book card image' />
             <Card.Body>
                 <Card.Title className="card-title">{title}</Card.Title>
-                <Button onclick={toggle}>More Info</Button>
+                <Button variant="outline-info" onClick={handleShow}>More Info</Button>
             </Card.Body>
-            <Modal>
+            <Modal show={show} onHide={handleClose} centered size="lg">
+                
+                <Modal.Header closeButton>
+                    <Modal.Title> {title} </Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    <img src={thumbnail} alt={title} />
+                    <p>Page Count: {pageCount} </p>
+                    <p>language: {language} </p>
+                    <p>Authors: {authors} </p>
+                    <p>Publisher: {publisher} </p>
+                </Modal.Body>
+                
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="outline-success" onClick={handleClose}>
+                        Preview Link
+                    </Button>
+                    <Button variant="outline-warning" onClick={handleClose}>
+                        Info Link
+                    </Button>
+                </Modal.Footer>
 
             </Modal>
         </Card>
