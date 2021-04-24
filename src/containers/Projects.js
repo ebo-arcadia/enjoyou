2996b3381707e430a135153bab50eeecdcfe7c1a
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-04-23 23:42:42 -0700

    changed branch name in local repo. adding redux to project components

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -1,15 +1,46 @@
-import React from 'react';
-import ProjectsList from '../components/ProjectsList'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchProjects } from '../actions/ProjectsActions';
+import ProjectsList from '../components/ProjectsList.js'
 
-const Projects = () => {
+class Projects extends Component {   
+
+  componentDidMount() {
+    console.log(this.props)
+    this.props.fetchProjects()
+  }
+
+  leadingPage = () => {
+    return this.props.loading
+  }
+
+  render() {
+    // debugger
+    console.log(this.props.catPics)
+    console.log(this.props.loading)
     return (
-        <div>
-            <h2>This is where to showcase your projects</h2>
-            <p>Git API to display cool projects on Github???</p>
-            {/* <ProjectsList /> */}
-        </div>
+      <div className="App">
+        <h1>CatBook</h1>
+        <ProjectsList catPics={this.props.catPics}/>
+        {this.leadingPage ? <h2>Loading cats images...</h2> : <h2>done loading!</h2>}
+      </div>
 
     );
+  }
 }
 
-export default Projects;
+const mapStateToProps = state => {
+  return {
+    catPics: state.cats,
+    loading: state.loading
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchProjects: () => dispatch(fetchProjects())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Projects)
+
--- src/projects-index.js
+++ src/projects-index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Projects from './containers/Projects';
+import AppProjects from './AppProjects';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk'
 import ProjectsReducer from './reducers/ProjectsReducer.js';
+import Projects from './containers/Projects.js';
 
 const store = createStore(ProjectsReducer, applyMiddleware(thunk))
 
-function AppProjects() {
-    <Provider store={store}>
-        <App-Projects />
-    </Provider>
-}
+ReactDOM.render(
+    <Provider context={Projects} store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+
 
