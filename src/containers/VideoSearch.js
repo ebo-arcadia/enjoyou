f0476d89db1ea7955837e0143c90fa7fa2fa8c80
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-04-24 21:31:59 -0700

    rapid youtube api fetching data working.

--- src/containers/VideoSearch.js
+++ src/containers/VideoSearch.js
@@ -7,8 +7,18 @@ const VideoSearch =() => {
 
     const [ SearchTerm, setSearchTerm ] = useState('')
 
-    const handleSubmit = () => {
-        console.log("whats up?")
+    async function handleSubmit() {
+        console.log(SearchTerm)
+        const response = await fetch(`https://youtube-search-results.p.rapidapi.com/youtube-search/?q=${SearchTerm}`, {
+            "method": "GET",
+            "headers": {
+            "x-rapidapi-host": "youtube-search-results.p.rapidapi.com",
+            "x-rapidapi-key": "931bd8e87fmsh0fd2ab219e7212ap1806bejsn8dd9a7988687"
+            }
+        });
+        const body = await response.json();
+        console.log(body);
+        return body.items.filter(item => item.type === 'video');
     }
 
     const searchForm = () => {
@@ -25,7 +35,7 @@ const VideoSearch =() => {
                             onChange = { e => setSearchTerm(e.target.value)}
                         />
                         <InputGroup.Append>
-                            <Button variant="warning" onClick={handleSubmit}>
+                            <Button variant="warning" onClick={ () => handleSubmit()}>
                                 <i className='fas fa-search'></i>
                             </Button>
                         </InputGroup.Append>
