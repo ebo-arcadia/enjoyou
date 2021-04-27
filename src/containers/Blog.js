8c6ff2c82aefc7e1f092e6b70914a3b39839a800
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-04-27 10:08:44 -0700

    added add article component. render data on rom inside blog component working

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function Blog() {
-    const article = [
+    const articles = [
         {id: 1, text: "Ultimate guide for React-Redux", posted: false },
         {id: 2, text: "Daily meditation", posted: false },
         {id: 3, text: "10 days with family in Maryland", posted: false }
@@ -10,9 +10,28 @@ export default function Blog() {
     return (
         <div>
             <h1>Write things that matter!</h1>
-            <BlogList article={article} />
+            <BlogList articles={articles} />
             <AddArticle />
         </div>
     );
-    
+
+    function BlogList( {articles} ) {
+        return (
+            <ul>
+                {articles.map((article) => (
+                    <li key={article.id}>{article.text}</li>
+                ))}
+            </ul>
+        );
+    }
+
+    function AddArticle() {
+        return (
+            <form>
+                <input placeholder="Add article" />
+                <button type="submit">Post</button>
+            </form>
+        )
+    }
+
 }
