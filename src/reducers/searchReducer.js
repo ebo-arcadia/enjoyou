118bb64c9328c99aea7622ddccd677d75e1d1b37
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-04-28 10:02:28 -0700

    search reducer adding intial state, pushes action payload into the initial state depending on if search_movie action type of fetch_movies action type

--- src/reducers/searchReducer.js
+++ src/reducers/searchReducer.js
@@ -1,2 +1,17 @@
-import { SEARCH_MOVIE } from '../actions/actionTypes'
+import { SEARCH_MOVIE, FETCH_MOVIES } from '../actions/actionTypes';
 
+const initialState = {
+    text: '',
+    movies: []
+}
+
+export default function(state = initialState, action) {
+    switch (action.type) {
+        case SEARCH_MOVIE:
+            return { ...state, text: action.payload }
+        case FETCH_MOVIES:
+            return { ...state, movies: action.payload }
+        default:
+            return state;
+    }
+}
