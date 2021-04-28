71c6454044c0f4b0ee3313bc391a6be891778b6f
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-04-27 21:02:58 -0700

    added color / light css from bootstrap

--- src/containers/Movies.js
+++ src/containers/Movies.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import MovieFooter from '../components/MovieFooter'
 import MovieHeader from '../components/MovieHeader'
+import '../style/movie.css'
 
 class Movies extends Component {
     render() {
