4d0144f025babb74da9a33d5a9898cb68464d744
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-04-28 15:18:05 -0700

    fixed empty video list passed to videolist component due to async of react by using ternay logic

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,54 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import VideoCard from './VideoCard';
 
-const VideoList = ({videoList}) => {
-    console.log(videoList)
-    if (videoList) {
-         videoList.map(video => {
-            return <VideoCard key={video.id} video={video} />
-        })
-    }
-    return <div></div>
+class VideoList extends Component {
+
+   render(){
+    let videos = this.props.videoList !== null ? this.props.videoList.map(video => <VideoCard video={video}/>) : " "
+        
+    return (
+        <div> {videos} </div>
+    )
+   }
 }
-
-// class VideoList extends Component {
-//     render() {
-//         const { videoList } = this.props;
-//         console.log(videoList)
-//         videoList.map(video => (
-//             return 
-//         ))
-//         return (
-//             videoList.map((video) => (
-//                 console.log(video.title)
-//                 <li key={video.id}>
-//                 )
-//             )
-//         )
-//         // })
-
-        // this.props.videoList.map((video) => {
-        //     console.log(video)
-        //     return (
-        //         <div className="video-card" key={video.id}>
-        //             < VideoCard 
-        //                 title={video.title}
-        //                 link={video.link}
-        //                 description={video.description}
-        //                 authorRef={video.author.ref}
-        //                 authorName={video.author.name}
-        //                 views={video.views}
-        //                 duration={video.duration}
-        //                 uploaded={video.uploaded_at}
-        //                 img={video.thumbnail}
-        //             />
-        //         </div>
-        //     )
-        // })
-
-//     }
-
-
-// }
 
 export default VideoList;
