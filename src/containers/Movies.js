5ac55f3ef8fc82a140eb7231b09eab73a1eb50b7
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-04-29 20:38:55 -0700

    adding loading to movies js ternary if loading then render loading spinner otherwise render movie list

--- src/containers/Movies.js
+++ src/containers/Movies.js
@@ -2,18 +2,27 @@ import React, { Component } from 'react';
 import MovieFooter from '../components/movie/movieFooter';
 import SearchMovieForm from '../components/movie/searchMovieForm';
 import MovieHeader from '../components/movie/movieHeader';
-import '../style/movie.css'
+import '../style/movie.css';
+import LoadingSpinner from '../components/movie/loadingSpinner';
+import MovieList from '../components/movie/movieList';
+import { connect } from 'react-redux';
 
 class Movies extends Component {
     render() {
+        const { loading } = this.props
         return (
             <div>
                 <MovieHeader />
                 <SearchMovieForm />
                 <MovieFooter />
+                {loading ? <LoadingSpinner /> : <MovieList />}
             </div>
         )
     }
 }
 
-export default Movies
+const mapStateToProps = state => ({
+    loading: state.movies.loading
+})
+
+export default connect(mapStateToProps)(Movies);
