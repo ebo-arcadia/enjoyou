457f98bc482bee8a5f4c554c60ecb6368694d74f
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-04-29 20:59:40 -0700

    conditionally map through movies state in movie list if movies list map movies then pass each movie to move card otherwise render an space

--- src/actions/MovieActions.js
+++ src/actions/MovieActions.js
@@ -11,7 +11,7 @@ export const searchMovie = text => dispatch => {
 export const fetchMovies = text => dispatch => {
     axios
     .get(`http://www.omdbapi.com/?i=tt3896198&apikey=719c52a0&s=${text}`)
-    .then(response => dispatch( { type: FETCH_MOVIES, payload: response.data.Search} ))
+    .then(response => dispatch( { type: FETCH_MOVIES, payload: response.data} ))
     .catch(error => alert(error))
 };
 
--- src/components/movie/movieList.js
+++ src/components/movie/movieList.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react';
+import MovieCard from './movieCard';
 
-const MovieList = (props) => {
-    return (
-        <div>
-            {/* {relay...} */}
-        </div>
-  
-    )    
+export class MovieList extends Component {
+    render() {
+        const { movies } = this.props;
+        let movieItems = movies.Response === 'True' ? movies.Search.map((movie, index) => <MovieCard key={index} movie={movie}/>) : " ";
+        return (
+            <div>{movieItems}</div>
+        )
+    }
 }
 
-export default MovieList
+const mapStateToProps = state => ({
+    movies: state.movies.movies
+})
+
+export default connect(mapStateToProps)(MovieList)
