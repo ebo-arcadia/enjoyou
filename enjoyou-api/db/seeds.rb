c35859c0b526bbf124c98baca864933c848287d6
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-05-02 23:01:43 -0400

    added namesapce routes. added seed data

--- enjoyou-api/db/seeds.rb
+++ enjoyou-api/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.create([
+    { title: 'Star Wars', year: "1973", imdbID: "r12421tt", poster: "img.com/1" }, 
+    { title: 'Lord of the Rings', year: "2000", imdbID: "r12sd2t", poster: "img.com/2" }, 
+    { title: 'The Matrix', year: "1999", imdbID: "r2ddw2s2t", poster: "img.com/3" }, 
+    ])
